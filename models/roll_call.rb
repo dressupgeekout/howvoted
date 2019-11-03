2beb1c9bb59617e50003b1d8c5078d07869710f3
Charlotte Koch <dressupgeekout@gmail.com>
2019-11-03 08:21:17 -0800

    There's a difference between HR and HRES

--- models/roll_call.rb
+++ models/roll_call.rb
@@ -2,6 +2,11 @@ class RollCall < Sequel::Model(:roll_calls)
   # XXX copypasta from the app
   CONSTITUTION_RATIFICATION_YEAR = 1787
 
+  FLAVORS = {
+    :BILL => 1,
+    :RESOLUTION => 2,
+  }
+
   # XXX copypasta from the app
   def ord(n)
     case n % 10
@@ -26,13 +31,28 @@ class RollCall < Sequel::Model(:roll_calls)
     return [n.to_s+ord(n), session.to_s+ord(session)]
   end
 
+  def flavor
+    return case self.legisnum
+    when /^H\ R\ /
+      FLAVORS[:BILL]
+    when /^H\ RES\ /
+      FLAVORS[:RESOLUTION]
+    else
+      nil
+    end
+  end
+
   def bill_link
-    if self.legisnum
-      nth, _ = self.year_to_congress
-      billno = self.legisnum.split(/\s+/).last
-      return "https://www.congress.gov/bill/#{nth}-congress/house-bill/#{billno}"
+    case flavor
+    when FLAVORS[:BILL]
+      dirname = "house-bill"
+    when FLAVORS[:RESOLUTION]
+      dirname = "house-resolution"
     else
       return nil
     end
+    nth, _ = self.year_to_congress
+    billno = self.legisnum.split(/\s+/).last
+    return "https://www.congress.gov/bill/#{nth}-congress/#{dirname}/#{billno}"
   end
 end
