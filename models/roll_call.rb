64e8e52f47fc3bac9ee4e9bcf5b583ca06d620e8
Charlotte Koch <dressupgeekout@gmail.com>
2019-11-03 00:08:56 -0700

    Clickable links to bill text. Buggy though. 2016 support.

--- models/roll_call.rb
+++ models/roll_call.rb
@@ -1,2 +1,38 @@
 class RollCall < Sequel::Model(:roll_calls)
+  # XXX copypasta from the app
+  CONSTITUTION_RATIFICATION_YEAR = 1787
+
+  # XXX copypasta from the app
+  def ord(n)
+    case n % 10
+    when 0
+      return "eth"
+    when 1
+      return "st"
+    when 2
+      return "nd"
+    when 3
+      return "rd"
+    else 
+      return "th"
+    end
+  end
+
+  # XXX copypasta from the app
+  def year_to_congress
+    y = self.year - CONSTITUTION_RATIFICATION_YEAR
+    n = y / 2
+    session = (y % 2) + 1
+    return [n.to_s+ord(n), session.to_s+ord(session)]
+  end
+
+  def bill_link
+    if self.legisnum
+      nth, _ = self.year_to_congress
+      billno = self.legisnum.split(/\s+/).last
+      return "https://www.congress.gov/bill/#{nth}-congress/house-bill/#{billno}"
+    else
+      return nil
+    end
+  end
 end
