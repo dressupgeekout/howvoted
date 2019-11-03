ba61674bbf4be4116782e7c1378eee570e88e0dc
Charlotte Koch <dressupgeekout@gmail.com>
2019-11-03 12:04:02 -0800

    Portraits of the reps! + jquery

--- models/legislator.rb
+++ models/legislator.rb
@@ -2,4 +2,12 @@ class Legislator < Sequel::Model(:legislators)
   def pretty
     return "#{self.full_name} (#{self.party}-#{self.state})"
   end
+
+  def portrait_url
+    return "http://bioguide.congress.gov/bioguide/photo/#{self.name_id[0]}/#{self.name_id}.jpg"
+  end
+
+  def portrait_img
+    return %Q(<img alt="" src="#{self.portrait_url}" height="225"/>)
+  end
 end
